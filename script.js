190c43151ab707524fa4a396f7840b9422fa4882
unknown <ADMIN@DESKTOP.cbc-ci.com>
2021-09-27 11:15:43 +0000

    new

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+// let sous;
+// sous = soustraction (7,8)
+
+
+
+function inkLevels(x){
+
+ let min = x.cyan
+ for (color in x)
+ if (x[color]< min){
+     min = x[color]
+ }
+
+
+}
+console.log(inkLevels);
